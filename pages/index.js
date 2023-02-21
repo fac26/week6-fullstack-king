216dd0259d0528e8f01b0077fe8c10141c6cfc48
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-02-21 17:29:28 +0000

    added data logic

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Head from 'next/head';
 import FruitList from '@/components/FruitList';
 import fruits from '@/lib/data';
 
-export default function Home() {
+export default function Home({ fruit_data }) {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
       <div>
-        <FruitList fruits={fruits} />
+        <FruitList fruits={fruit_data} />
         <Link href="/fruits/berries">Berries</Link>
         <Link href="/fruits/citrus">Citrus</Link>
       </div>
@@ -18,13 +18,12 @@ export default function Home() {
   );
 }
 
-// export function getStaticProps() {
-//   const allFruits = fruits;
-
-//   console.log(allFruits);
-//   return {
-//     props: {
-//       fruit_data: allFruits,
-//     },
-//   };
-// }
+export function getStaticProps() {
+  const allFruits = fruits;
+  console.log(allFruits);
+  return {
+    props: {
+      fruit_data: allFruits,
+    },
+  };
+}
