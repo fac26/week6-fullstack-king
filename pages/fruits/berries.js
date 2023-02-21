4b1e5de618b016c25f18d332eb8bedde238fdd04
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-02-21 17:38:56 +0000

    berries page

--- pages/fruits/berries.js
+++ pages/fruits/berries.js
@@ -1,3 +1,26 @@
-export default function Berries() {
-  return <>Berries list here</>;
+import Head from 'next/head';
+import FruitList from '@/components/FruitList';
+import fruits from '@/lib/data';
+
+export default function Berries({ fruit_data }) {
+  return (
+    <>
+      <Head>
+        <title>Berries</title>
+      </Head>
+      <div>
+        <FruitList fruits={fruit_data} />
+      </div>
+    </>
+  );
+}
+
+export function getStaticProps() {
+  const allFruits = fruits.filter((fruit) => fruit.fruit_type === 'Berries');
+
+  return {
+    props: {
+      fruit_data: allFruits,
+    },
+  };
 }
