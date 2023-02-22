867c779d0e0fa17d836a7cea35c5e793e11adcc1
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-02-22 15:59:20 +0000

    destructured props

--- components/Fruit.js
+++ components/Fruit.js
@@ -1,36 +1,24 @@
-//individual fruit page
-//more info on the fruit like description, price and add to cart option
-//need to import image
-
 import Image from 'next/image';
 import Button from './Button';
 import ctx from '@/store/ctx-obj';
-import React from 'react';
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 
-export default function Fruitpage(props) {
+export default function FruitPage(props) {
+  const { title, image_path, description, price } = props;
+  const addToBasket = () => cartCTX.addItem(props);
   const cartCTX = useContext(ctx);
-  const addToBasket = () => {
-    console.log('added to basket ', props);
 
-    cartCTX.addItem(props);
-  };
   return (
     <>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
       <div>
-        <Image
-          src={props.image_path}
-          alt={props.title}
-          width="300"
-          height="300"
-        />
+        <Image src={image_path} alt={title} width="300" height="300" />
       </div>
       <div>
-        <p>{props.description}</p>
+        <p>{description}</p>
       </div>
       <div>
-        <p>£ {props.price}</p>
+        <p>£ {price}</p>
       </div>
       <Button onClick={addToBasket}>Add to Cart</Button>
     </>
